a24d6bd42ad9bb921ccfae8d46bceaa9a9430de2
adrian <adrian@fury.fan>
2023-05-27 21:12:38 +0530

    comit

--- src/utils/contants.tsx
+++ src/utils/contants.tsx
@@ -1,5 +1,5 @@
 export const REACT_APP_BACKEND_URL =
-    process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000';
+    process.env.REACT_APP_BACKEND_URL || 'http://34.100.244.151:8000';
 
 export const REACT_APP_NODE_URL =
     process.env.REACT_APP_NODE_URL || 'https://api.fury.black';
